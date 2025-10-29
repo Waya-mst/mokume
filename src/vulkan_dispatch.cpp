3d1fdc5419cdc4b6399d97a35e214c619ac697bf
Waya-mst <kameeeeei1234@gmail.com>
2025-10-30 04:16:05 +0900

    fix order vulkan_dispatch.cpp

--- src/vulkan_dispatch.cpp
+++ src/vulkan_dispatch.cpp
@@ -1,5 +1,5 @@
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
-#define VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 #include <vulkan/vulkan.hpp>
+#define VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 
 #pragma message("** compiling vulkan_dispatch.cpp **")
